e8dc78274882409019d830471b2818efc8644418
Pedro-Miguel-Correia <pedromsantoscorreia@gmail.com>
2026-03-25 17:38:40 +0000

    prisma config
    prisma and db config

--- pinder-backend/src/app.js
+++ pinder-backend/src/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 // const router = require('./routes'); // Descomente quando criar o index.js em routes
 
 const app = express();
@@ -9,18 +9,18 @@ app.use(cors()); // Permite que o seu front-end acesse a API
 app.use(express.json()); // Permite que o app entenda corpo de requisição em JSON
 
 // --- Rotas ---
-// app.use('/api', router); 
+// app.use('/api', router);
 
 // Rota de teste inicial
-app.get('/', (req, res) => {
-  res.status(200).send({ message: "Pinder API está rodando! 🚀" });
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Pinder API is running! 🚀" });
 });
 
 // --- Exportação ---
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
