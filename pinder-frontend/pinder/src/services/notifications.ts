fb4120879eee096dd265ea8cf57ce2c2be654a22
Pedro-Miguel-Correia <pedromsantoscorreia@gmail.com>
2026-05-24 16:02:49 +0100

    notifications logic

---
+++ pinder-frontend/pinder/src/services/notifications.ts
@@ -0,0 +1,31 @@
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+
+export async function registerForPushNotifications() {
+
+  if (!Device.isDevice) {
+    return null;
+  }
+
+  const { status: existingStatus } =
+    await Notifications.getPermissionsAsync();
+
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== 'granted') {
+
+    const { status } =
+      await Notifications.requestPermissionsAsync();
+
+    finalStatus = status;
+  }
+
+  if (finalStatus !== 'granted') {
+    return null;
+  }
+
+  const tokenData =
+    await Notifications.getExpoPushTokenAsync();
+
+  return tokenData.data;
+}
